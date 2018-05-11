afcd64cf83ebde7d44b502c2d5d48b886526f21e
Raymond Maquiso <raymacz76@gmail.com>
2018-05-11 18:23:38 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * A simple sandbox.
+ */
+?>
+
+<html>
+	<head>
+		<title>Variable Variables</title>
+	</head>
+	<body>
+	<?php
+        // code here
+        include_once 'class_example5.php';
+	?>
+	</body>
+</html>
